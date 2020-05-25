7e3c0d913f21f8be6fd07ea385b5e4cd69d2f754
damian212121 <nico.damian.ex@gmail.com>
2020-05-24 22:05:35 -0300

    Configuracion de gulp para compilar scss

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+function css() {
+    return gulp
+        .src('./scss/app.scss')
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: ['last 2 versions'],
+                cascade: false,
+            })
+        )
+        .pipe(
+            sass({
+                outputStyle: 'expanded',
+            })
+        )
+        .pipe(gulp.dest('css'));
+}
+
+function wathcFiles() {
+    gulp.watch('scss/*.scss', css);
+    gulp.watch('index.html');
+}
+// Registrar  funciones como tareas
+
+gulp.task('css', css);
+gulp.task('watch', gulp.parallel(wathcFiles));
